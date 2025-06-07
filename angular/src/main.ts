5893b29734b024c447f6986ffbe5846fadc5841f
John Williams <zdu.strong@gmail.com>
2025-06-07 10:01:43 +0900

    Remove angular project

--- angular/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './common/config/app.config';
-import { Home } from './app/home/home/home';
-
-bootstrapApplication(Home, appConfig)
-  .catch((err) => console.error(err));
