592a20debacdd560bb2e1d9c005a973335b47216
John Williams <zdu.strong@gmail.com>
2025-07-18 06:36:54 +0900

    #: Modify BaseController

--- springboot/src/main/java/com/john/project/common/baseController/BaseController.java
+++ springboot/src/main/java/com/john/project/common/baseController/BaseController.java
@@ -24,7 +24,7 @@ import jakarta.servlet.http.HttpServletResponse;
  *
  */
 @RestController
-public class BaseController {
+public abstract class BaseController {
 
     @Autowired
     protected HttpServletRequest request;
