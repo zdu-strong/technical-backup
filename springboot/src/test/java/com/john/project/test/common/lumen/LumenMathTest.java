fdff6768877ffdeeb9a1cb6b3edcfe008b4d2fd4
John Williams <zdu.strong@gmail.com>
2025-07-27 15:11:43 +0900

    Modify unit tests

---
+++ springboot/src/test/java/com/john/project/test/common/lumen/LumenMathTest.java
@@ -0,0 +1,100 @@
+package com.john.project.test.common.lumen;
+
+import com.john.project.test.common.BaseTest.BaseTest;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+public class LumenMathTest extends BaseTest {
+
+    private final String usdCurrencyBalance = "66.66";
+
+    private final String japanCurrencyBalance = "200";
+
+    private final String usdCcuBalance = "150";
+
+    private final String japanCcuBalance = "50";
+
+    private final String injectUsdCurrencyBalance = "233.33";
+    private final String injectJapanCurrencyBalance = "100";
+
+    private final String amountNeedToExchangeUsdCurrencyBalance = "x";
+
+    // z = x * 150 / (x + 66.66) ccu
+    private final String exchangeCcuBalance = "z";
+
+    // y = z * 200 / (50 + z )
+    private final String exchangeJapanCurrencyBalance = "y";
+
+    private final String equalSymbol = "=";
+    public static final String addSymbol = "+";
+    public static final String subtractSymbol = "-";
+    public static final String multipleSymbol = "*";
+    public static final String divideSymbol = "/";
+
+    @Test
+    public void test() {
+        var aa = init();
+        var cc = aa.toString();
+        "".toString();
+    }
+
+    private MathBracketModel init() {
+        // (injectUsdCurrencyBalance - amountNeedToExchangeUsdCurrencyBalance) * (usdCcuBalance - exchangeCcuBalance) / (usdCurrencyBalance + amountNeedToExchangeUsdCurrencyBalance) = (injectJapanCurrencyBalance + exchangeJapanCurrencyBalance) *  (japanCcuBalance + exchangeCcuBalance) / (japanCurrencyBalance - exchangeJapanCurrencyBalance)
+        return new MathBracketModel()
+                .setChildOne(
+                        new MathBracketModel()
+                                .setChildOne(
+                                        new MathBracketModel()
+                                                .setChildOne(
+                                                        new MathBracketModel()
+                                                                .setChildOne(new MathBracketModel().setName(injectUsdCurrencyBalance))
+                                                                .setCalculationSymbol(subtractSymbol)
+                                                                .setChildTwo(new MathBracketModel().setName(amountNeedToExchangeUsdCurrencyBalance))
+                                                )
+                                                .setCalculationSymbol(multipleSymbol)
+                                                .setChildTwo(
+                                                        new MathBracketModel()
+                                                                .setChildOne(new MathBracketModel().setName(usdCcuBalance))
+                                                                .setCalculationSymbol(subtractSymbol)
+                                                                .setChildTwo(new MathBracketModel().setName(exchangeCcuBalance))
+                                                )
+                                )
+                                .setCalculationSymbol(divideSymbol)
+                                .setChildTwo(
+                                        new MathBracketModel()
+                                                .setChildOne(new MathBracketModel().setName(usdCurrencyBalance))
+                                                .setCalculationSymbol(addSymbol)
+                                                .setChildTwo(new MathBracketModel().setName(amountNeedToExchangeUsdCurrencyBalance))
+                                )
+                )
+                .setCalculationSymbol(equalSymbol)
+                .setChildTwo(
+                        new MathBracketModel()
+                                .setChildOne(
+                                        new MathBracketModel()
+                                                .setChildOne(
+                                                        new MathBracketModel()
+                                                                .setChildOne(new MathBracketModel().setName(injectJapanCurrencyBalance))
+                                                                .setCalculationSymbol(addSymbol)
+                                                                .setChildTwo(new MathBracketModel().setName(exchangeJapanCurrencyBalance))
+                                                )
+                                                .setCalculationSymbol(multipleSymbol)
+                                                .setChildTwo(
+                                                        new MathBracketModel()
+                                                                .setChildOne(new MathBracketModel().setName(japanCcuBalance))
+                                                                .setCalculationSymbol(addSymbol)
+                                                                .setChildTwo(new MathBracketModel().setName(exchangeCcuBalance))
+                                                )
+                                )
+                                .setCalculationSymbol(divideSymbol)
+                                .setChildTwo(
+                                        new MathBracketModel()
+                                                .setChildOne(new MathBracketModel().setName(japanCurrencyBalance))
+                                                .setCalculationSymbol(subtractSymbol)
+                                                .setChildTwo(new MathBracketModel().setName(exchangeJapanCurrencyBalance))
+                                )
+                );
+    }
+
+}
---
+++ springboot/src/test/java/com/john/project/test/common/lumen/MathBracketModel.java
@@ -0,0 +1,62 @@
+package com.john.project.test.common.lumen;
+
+
+import cn.hutool.core.text.StrFormatter;
+import cn.hutool.core.util.ObjectUtil;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.john.project.test.common.lumen.LumenMathTest.addSymbol;
+import static com.john.project.test.common.lumen.LumenMathTest.multipleSymbol;
+
+@Getter
+@Setter
+@Accessors(chain = true)
+public class MathBracketModel {
+
+    private String id;
+
+    private String name;
+
+    private MathBracketModel childOne;
+
+    private String calculationSymbol;
+
+    private MathBracketModel childTwo;
+
+    @Override
+    public String toString() {
+        if (StringUtils.isNotBlank(name)) {
+            return name;
+        }
+        if (ObjectUtil.isNotNull(childOne) && ObjectUtil.isNotNull(childTwo) && StringUtils.isNotBlank(calculationSymbol)) {
+            var hasBracketSymbolOfChildOne = !(List.of(addSymbol, multipleSymbol).contains(calculationSymbol) && ObjectUtil.equals(calculationSymbol, childOne.getCalculationSymbol()));
+            var hasBracketSymbolOfChildTwo = !(List.of(addSymbol, multipleSymbol).contains(calculationSymbol) && ObjectUtil.equals(calculationSymbol, childTwo.getCalculationSymbol()));
+            if (StringUtils.isNotBlank(childOne.getName())) {
+                hasBracketSymbolOfChildOne = false;
+            }
+            if (StringUtils.isNotBlank(childTwo.getName())) {
+                hasBracketSymbolOfChildTwo = false;
+            }
+            return StrFormatter.format(
+                    "{}{}{} {} {}{}{}",
+                    Optional.of(hasBracketSymbolOfChildOne).filter(s -> s).map(s -> "(").orElse(StringUtils.EMPTY),
+                    childOne.toString(),
+                    Optional.of(hasBracketSymbolOfChildOne).filter(s -> s).map(s -> ")").orElse(StringUtils.EMPTY),
+                    calculationSymbol,
+                    Optional.of(hasBracketSymbolOfChildTwo).filter(s -> s).map(s -> "(").orElse(StringUtils.EMPTY),
+                    childTwo.toString(),
+                    Optional.of(hasBracketSymbolOfChildTwo).filter(s -> s).map(s -> ")").orElse(StringUtils.EMPTY)
+            );
+        }
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "no match model");
+    }
+
+}
