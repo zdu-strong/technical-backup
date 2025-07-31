b24ef78e1c34f945911d2369b848dc44b8ef1bfb
John Williams <zdu.strong@gmail.com>
2025-07-31 19:55:05 +0900

    Modify WebPathMatchTrailingSlashConfig

--- springboot/src/main/java/com/john/project/common/config/WebPathMatchTrailingSlashConfig.java
+++ springboot/src/main/java/com/john/project/common/config/WebPathMatchTrailingSlashConfig.java
@@ -1,16 +1,25 @@
 package com.john.project.common.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.filter.UrlHandlerFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebPathMatchTrailingSlashConfig implements WebMvcConfigurer {
 
-    @SuppressWarnings("deprecation")
-    @Override
-    public void configurePathMatch(PathMatchConfigurer configurer) {
-        configurer.setUseTrailingSlashMatch(true);
+    @Bean
+    public FilterRegistrationBean<UrlHandlerFilter> urlHandlerFilterRegistration() {
+        UrlHandlerFilter filter = UrlHandlerFilter
+                .trailingSlashHandler("/**")
+                .redirect(HttpStatus.PERMANENT_REDIRECT)
+                .build();
+        FilterRegistrationBean<UrlHandlerFilter> registration = new FilterRegistrationBean<>(filter);
+        registration.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return registration;
     }
 
 }
