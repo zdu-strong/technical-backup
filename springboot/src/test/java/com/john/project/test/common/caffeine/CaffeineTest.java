bbc061bf60026d4cbef07034796058fc1ad16895
John Williams <zdu.strong@gmail.com>
2025-08-22 20:33:17 +0900

    Add unit tests

---
+++ springboot/src/test/java/com/john/project/test/common/caffeine/CaffeineTest.java
@@ -0,0 +1,41 @@
+package com.john.project.test.common.caffeine;
+
+import cn.hutool.core.date.TimeInterval;
+import cn.hutool.core.thread.ThreadUtil;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.john.project.test.common.BaseTest.BaseTest;
+import lombok.SneakyThrows;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static com.john.project.constant.HelloWorldConstant.HELLO_WORLD;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CaffeineTest extends BaseTest {
+
+    @Test
+    @SneakyThrows
+    public void test() {
+        var caffeineLoadCache = Caffeine.newBuilder()
+                .expireAfterWrite(30, TimeUnit.SECONDS)
+                .refreshAfterWrite(29, TimeUnit.SECONDS)
+                .buildAsync((key) -> {
+                    return query();
+                });
+        caffeineLoadCache.get(StringUtils.EMPTY).get();
+        var timer = new TimeInterval();
+        for (var i = 1000 * 1000; i > 0; i--) {
+            caffeineLoadCache.get(StringUtils.EMPTY).get();
+        }
+        var costTimes = timer.interval();
+        assertTrue(costTimes < 200);
+    }
+
+    private String query() {
+        ThreadUtil.sleep(100);
+        return HELLO_WORLD;
+    }
+
+}
