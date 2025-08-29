bd0b8baff3d802415eeb44e71a6266691cc20eff
John Williams <zdu.strong@gmail.com>
2025-08-29 21:20:10 +0900

    Modify CaffeineQueryBalanceTest

--- springboot/src/test/java/com/john/project/test/common/caffeine/CaffeineQueryBalanceTest.java
+++ springboot/src/test/java/com/john/project/test/common/caffeine/CaffeineQueryBalanceTest.java
@@ -1,15 +1,12 @@
 package com.john.project.test.common.caffeine;
 
 import cn.hutool.core.date.TimeInterval;
-import cn.hutool.core.text.StrFormatter;
 import cn.hutool.core.thread.ThreadUtil;
 import com.github.benmanes.caffeine.cache.Caffeine;
-import com.john.project.constant.DateFormatConstant;
 import com.john.project.test.common.BaseTest.BaseTest;
 import io.reactivex.rxjava3.core.Flowable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import lombok.SneakyThrows;
-import org.apache.commons.lang3.time.FastDateFormat;
 import org.junit.jupiter.api.Test;
 
 import java.util.Date;
@@ -21,22 +18,22 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CaffeineQueryBalanceTest extends BaseTest {
 
+    private final long intervalMilliseconds = 10;
+
     @Test
     @SneakyThrows
     public void test() {
         var caffeineLoadCache = Caffeine.newBuilder()
                 .executor(Executors.newVirtualThreadPerTaskExecutor())
                 .expireAfterAccess(3, TimeUnit.SECONDS)
-                .buildAsync((String key) -> {
-                    return queryBalance(key);
-                });
+                .buildAsync((String key) -> queryBalance());
         var timer = new TimeInterval();
-        Flowable.range(0, 1000 * 10)
+        Flowable.range(1, 1000 * 10)
                 .parallel(1000 * 10)
-                .runOn(Schedulers.from(Executors.newVirtualThreadPerTaskExecutor()))
+                .runOn(Schedulers.from(applicationTaskExecutor))
                 .doOnNext((s) -> {
                     ThreadUtil.sleep(10);
-                    caffeineLoadCache.get(FastDateFormat.getInstance(StrFormatter.format("{}.SS", DateFormatConstant.YEAR_MONTH_DAY_HOUR_MINUTE_SECOND)).format(new Date())).get();
+                    caffeineLoadCache.get(String.valueOf(new Date().getTime() / intervalMilliseconds)).get();
                 })
                 .sequential()
                 .blockingSubscribe();
@@ -44,7 +41,7 @@ public class CaffeineQueryBalanceTest extends BaseTest {
         assertTrue(costTimes < 500);
     }
 
-    private String queryBalance(String queryDateString) {
+    private String queryBalance() {
         ThreadUtil.sleep(100);
         return HELLO_WORLD;
     }
