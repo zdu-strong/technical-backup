36e084c38fc876daad46ed1188aec0114302dc21
John Williams <zdu.strong@gmail.com>
2025-08-30 16:00:26 +0900

    Modify CaffeineQueryBalanceTest

--- springboot/src/test/java/com/john/project/test/common/caffeine/CaffeineQueryBalanceTest.java
+++ springboot/src/test/java/com/john/project/test/common/caffeine/CaffeineQueryBalanceTest.java
@@ -2,6 +2,7 @@ package com.john.project.test.common.caffeine;
 
 import cn.hutool.core.date.TimeInterval;
 import cn.hutool.core.thread.ThreadUtil;
+import com.github.benmanes.caffeine.cache.AsyncLoadingCache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.john.project.test.common.BaseTest.BaseTest;
 import io.reactivex.rxjava3.core.Flowable;
@@ -9,7 +10,7 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 
-import java.util.Date;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import static com.john.project.constant.HelloWorldConstant.HELLO_WORLD;
@@ -18,29 +19,32 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class CaffeineQueryBalanceTest extends BaseTest {
 
     private final long intervalMilliseconds = 10;
+    private final AsyncLoadingCache<String, String> caffeineLoadCache = Caffeine.newBuilder()
+            .executor(Executors.newVirtualThreadPerTaskExecutor())
+            .expireAfterAccess(3, TimeUnit.SECONDS)
+            .buildAsync(this::queryBalanceFromDatabase);
 
     @Test
     @SneakyThrows
     public void test() {
-        var caffeineLoadCache = Caffeine.newBuilder()
-                .executor(applicationTaskExecutor)
-                .expireAfterAccess(3, TimeUnit.SECONDS)
-                .buildAsync((String key) -> queryBalance());
         var timer = new TimeInterval();
         Flowable.range(1, 1000 * 10)
                 .parallel(1000 * 10)
                 .runOn(Schedulers.from(applicationTaskExecutor))
-                .doOnNext((s) -> {
-                    ThreadUtil.sleep(10);
-                    caffeineLoadCache.get(String.valueOf(new Date().getTime() / intervalMilliseconds)).get();
-                })
+                .doOnNext((s) -> getBalance())
                 .sequential()
                 .blockingSubscribe();
         var costTimes = timer.interval();
         assertTrue(costTimes < 500);
     }
 
-    private String queryBalance() {
+    @SneakyThrows
+    private String getBalance() {
+        ThreadUtil.sleep(intervalMilliseconds);
+        return caffeineLoadCache.get(String.valueOf(System.currentTimeMillis() / intervalMilliseconds)).get();
+    }
+
+    private String queryBalanceFromDatabase(String key) {
         ThreadUtil.sleep(100);
         return HELLO_WORLD;
     }
