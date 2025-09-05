a9f596cf19ac178ea6f4055fad8aef09b6ca8b15
John Williams <zdu.strong@gmail.com>
2025-09-06 06:20:37 +0900

    Modify typescript type file of the react project

--- react/src/common/typescript-files/index.d.ts
+++ react/src/common/typescript-files/index.d.ts
@@ -7,3 +7,10 @@ declare module "*.svg" {
   const content: string;
   export default content;
 }
+
+declare module "*.gltf" {
+  const content: string;
+  export default content;
+}
+
+declare module '@/*';
