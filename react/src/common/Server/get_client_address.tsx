c54e2c10e74d9df6938911dfc8fe1040c6af3a03
John Williams <zdu.strong@gmail.com>
2025-09-05 23:55:59 +0900

    Modify ClientAddress

--- react/src/common/Server/get_client_address.tsx
+++ react/src/common/Server/get_client_address.tsx
@@ -3,7 +3,10 @@ import { existsWindow } from '@common/exists-window/exists-window';
 let ClientAddress = 'http://127.0.0.1:3000';
 
 if (existsWindow) {
-  ClientAddress = window.location.origin;
+  var origin = window.location.origin;
+  if (origin.startsWith("http")) {
+    ClientAddress = origin;
+  }
 }
 
 export { ClientAddress }
