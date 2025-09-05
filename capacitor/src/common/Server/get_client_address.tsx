f2c2bfe74035aa833c5c323c14a841250cb02fed
John Williams <zdu.strong@gmail.com>
2025-09-06 06:45:33 +0900

    Modify get_client_address file of the react project

--- capacitor/src/common/Server/get_client_address.tsx
+++ capacitor/src/common/Server/get_client_address.tsx
@@ -3,7 +3,7 @@ import { existsWindow } from '@common/exists-window/exists-window';
 let ClientAddress = 'http://127.0.0.1:3000';
 
 if (existsWindow) {
-  var origin = window.location.origin;
+  const origin = window.location.origin;
   if (origin.startsWith("http")) {
     ClientAddress = origin;
   }
