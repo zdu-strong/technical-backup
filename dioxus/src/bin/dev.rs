1e53409c226957ed477cd688bc18d738722d3a13
john <john@localhost.localdomain>
2025-09-22 23:25:38 -0400

    Modify cargo dev command

--- dioxus/src/bin/dev.rs
+++ dioxus/src/bin/dev.rs
@@ -2,7 +2,7 @@ use std::process::Command;
 
 fn main() {
     Command::new("dx")
-        .args(["serve", "--hot-reload=true", "--open=true", "--port=3000", "--platform=web", "--cross-origin-policy"])
+        .args(["serve", "--hot-reload=true", "--open=true", "--port=3000", "--platform=web"])
         .status()
         .unwrap();
 }
