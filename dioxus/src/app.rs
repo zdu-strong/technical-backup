61da725c6616e56f14a62203f850037825d2609c
john <john@localhost.localdomain>
2025-09-24 22:15:57 +0900

    Add google robot fonts

--- dioxus/src/app.rs
+++ dioxus/src/app.rs
@@ -5,15 +5,17 @@ use dioxus_material::Theme;
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/styling/main.css");
 const TAILWIND_JS: Asset = asset!("/assets/tailwind.js");
+const GOOGLE_ROBOT_FONT_CSS: Asset = asset!("/assets/google_robot_font.css");
+const GOOGLE_MATERIAL_CSS: Asset = asset!("/assets/google_material.css");
 
 #[component]
 pub fn App() -> Element {
     rsx! {
         document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS }
-        document::Script { src: TAILWIND_JS}
-        Theme {
-          Router::<Route> {}
-        }
+        document::Link { rel: "stylesheet", href: GOOGLE_ROBOT_FONT_CSS }
+        document::Link { rel: "stylesheet", href: GOOGLE_MATERIAL_CSS }
+        document::Script { src: TAILWIND_JS }
+        Theme { Router::<Route> {} }
     }
 }
