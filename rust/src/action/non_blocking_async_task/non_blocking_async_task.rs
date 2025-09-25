d2fc31ff60c91e1568ccd20d72d9ecae0aa295e2
john <john@localhost.localdomain>
2025-09-26 08:10:46 +0900

    Modify example of spawn_blocking

--- rust/src/action/non_blocking_async_task/non_blocking_async_task.rs
+++ rust/src/action/non_blocking_async_task/non_blocking_async_task.rs
@@ -3,6 +3,9 @@ use tokio::task::spawn_blocking;
 pub async fn non_blocking_async_task() {
     let _ = spawn_blocking(move || async move {
         // heavy_cpu
+        return "Hello World".to_string();
     })
+    .await
+    .unwrap()
     .await;
 }
