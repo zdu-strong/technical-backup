0174dcbf66c655fc158d8f0beb7192fb10965d6c
john <john@localhost.localdomain>
2025-09-26 08:29:05 +0900

    Modify example of global variable

--- rust/src/action/print_global_username/print_username.rs
+++ rust/src/action/print_global_username/print_username.rs
@@ -1,7 +1,7 @@
 use crate::constant::global_user_constant::GLOBAL_USERNAME;
 
 pub async fn print_username() {
-    let ref mut username = *GLOBAL_USERNAME.lock().await;
-    *username = "Jerry".to_string();
+    *GLOBAL_USERNAME.write().await = "Jerry".to_string();
+    let username = GLOBAL_USERNAME.read().await;
     println!("My name is {:?}", username);
 }
--- rust/src/constant/global_user_constant.rs
+++ rust/src/constant/global_user_constant.rs
@@ -1,6 +1,7 @@
+use std::sync::Arc;
 use lazy_static::lazy_static;
-use tokio::sync::Mutex;
+use tokio::sync::RwLock;
 
 lazy_static! {
-    pub static ref GLOBAL_USERNAME: Mutex<String> = Mutex::new("tom".to_string());
+    pub static ref GLOBAL_USERNAME: Arc<RwLock<String>> = Arc::new(RwLock::new("tom".to_string()));
 }
