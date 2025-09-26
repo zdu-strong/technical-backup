c0e5b00aa52f3005a20cd14b59432fd2e762ed5c
john <john@localhost.localdomain>
2025-09-26 22:08:58 +0900

    Add once_cell dependency

--- rust/src/constant/global_user_constant.rs
+++ rust/src/constant/global_user_constant.rs
@@ -1,7 +1,5 @@
+use once_cell::sync::Lazy;
 use std::sync::Arc;
-use lazy_static::lazy_static;
 use tokio::sync::RwLock;
 
-lazy_static! {
-    pub static ref GLOBAL_USERNAME: Arc<RwLock<String>> = Arc::new(RwLock::new("tom".to_string()));
-}
+pub static GLOBAL_USERNAME: Lazy<Arc<RwLock<String>>> = Lazy::new(|| Arc::new(RwLock::new("tom".to_string())));
