de930965d90e1fa7771cfd9ba9a89b841caa87b3
john <john@localhost.localdomain>
2025-09-27 17:18:10 +0900

    Modify blog example

--- dioxus/src/route/blog.rs
+++ dioxus/src/route/blog.rs
@@ -3,21 +3,26 @@ use dioxus::prelude::*;
 
 const BLOG_CSS: Asset = asset!("/assets/styling/blog.css");
 
+#[derive(Debug, Clone, Copy, Props, PartialEq)]
+pub struct Props {
+    pub id: i32,
+}
+
 #[component]
-pub fn Blog(id: i32) -> Element {
+pub fn Blog(props: Props) -> Element {
     rsx! {
         document::Link { rel: "stylesheet", href: BLOG_CSS }
 
         div { id: "blog",
 
-            h1 { "This is blog #{id}!" }
+            h1 { "This is blog #{props.id}!" }
             p {
-                "In blog #{id}, we show how the Dioxus router works and how URL parameters can be passed as props to our route components."
+                "In blog #{props.id}, we show how the Dioxus router works and how URL parameters can be passed as props to our route components."
             }
 
-            Link { to: Route::Blog { id: id - 1 }, "Previous" }
+            Link { to: Route::Blog { id: props.id - 1 }, "Previous" }
             span { " <---> " }
-            Link { to: Route::Blog { id: id + 1 }, "Next" }
+            Link { to: Route::Blog { id: props.id + 1 }, "Next" }
         }
     }
 }
