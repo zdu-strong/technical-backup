a9e3876c69f9e1a15f9020592398449636d145a3
John Williams <zdu.strong@gmail.com>
2025-10-07 14:06:31 +0900

    Modify hero component

--- dioxus/src/component/hero.rs
+++ dioxus/src/component/hero.rs
@@ -31,7 +31,7 @@ pub fn Hero() -> Element {
             update_date: Signal::new(Local::now()),
         }];
         let ref mut json_string = serde_json::to_string(user_list).unwrap();
-        info!("user_list_json_string: {:?}", json_string);
+        info!("user_list_json_string: {}", json_string);
         let ref mut user_list: Vec<UserModel> = serde_json::from_str(json_string).unwrap();
         info!("user_list: {:?}", user_list);
     });
