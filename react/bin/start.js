c3d7cb1089e8f80d9bbe815dd774d6a7a9720a93
John Williams <zdu.strong@gmail.com>
2025-10-08 18:15:22 +0900

    format code of react project

--- react/bin/start.js
+++ react/bin/start.js
@@ -2,22 +2,22 @@ const execa = require("execa")
 const path = require("path")
 
 async function main() {
-  await startClient();
-  process.exit();
+    await startClient();
+    process.exit();
 }
 
 async function startClient() {
-  await execa.command(
-    "rsbuild dev",
-    {
-      stdio: "inherit",
-      cwd: path.join(__dirname, ".."),
-      extendEnv: true,
-      env: {
-        "RSBUILD_OPEN": process.env.RSBUILD_PORT || true
-      }
-    }
-  );
+    await execa.command(
+        "rsbuild dev",
+        {
+            stdio: "inherit",
+            cwd: path.join(__dirname, ".."),
+            extendEnv: true,
+            env: {
+                "RSBUILD_OPEN": process.env.RSBUILD_PORT || true
+            }
+        }
+    );
 }
 
 module.exports = main()
