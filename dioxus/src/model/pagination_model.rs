da636de1d2d2989ca08c101ff7326749ba8cc97a
John Williams <zdu.strong@gmail.com>
2025-11-06 10:01:02 +0900

    Modify PaginationModel of dioxus project

--- dioxus/src/model/pagination_model.rs
+++ dioxus/src/model/pagination_model.rs
@@ -17,7 +17,7 @@ where
 }
 
 impl<T> PaginationModel<T> {
-    pub fn new(page_num: i128, page_size: i128, items: Vec<T>) -> PaginationModel<T> {
+    pub fn from(page_num: i128, page_size: i128, items: Vec<T>) -> PaginationModel<T> {
         if page_num < 1 {
             panic!("page_num cannot be less than 1")
         }
@@ -38,6 +38,31 @@ impl<T> PaginationModel<T> {
                     .skip(((page_num - 1) * page_size).to_usize().unwrap())
                     .take(page_size.to_usize().unwrap())
                     .map(|item| Signal::new(item))
+                    .collect::<Vec<Signal<_>>>(),
+            ),
+        }
+    }
+
+    pub fn new(page_num: i128, page_size: i128, items: Vec<Signal<T>>) -> PaginationModel<T> {
+        if page_num < 1 {
+            panic!("page_num cannot be less than 1")
+        }
+        if page_size < 1 {
+            panic!("page_size cannot be less than 1")
+        }
+        let total_records = items.len().to_i128().unwrap();
+        let total_pages =
+            (total_records / page_size) + (if total_records % page_size > 0 { 1 } else { 0 });
+        PaginationModel {
+            page_num: Signal::new(page_num),
+            page_size: Signal::new(page_size),
+            total_records: Signal::new(total_records),
+            total_pages: Signal::new(total_pages),
+            items: Signal::new(
+                items
+                    .into_iter()
+                    .skip(((page_num - 1) * page_size).to_usize().unwrap())
+                    .take(page_size.to_usize().unwrap())
                     .collect::<Vec<Signal<_>>>(),
             ),
         }
