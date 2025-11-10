2e7b701a3d225a73bc35b19d65933437caf827b6
John Williams <zdu.strong@gmail.com>
2025-11-10 17:34:18 +0900

    Add google roboto font of dioxus project

--- dioxus/src/app.rs
+++ dioxus/src/app.rs
@@ -6,14 +6,12 @@ const MAIN_CSS: Asset = asset!("/assets/styling/main.css");
 const APP_CSS: Asset = asset!("/assets/styling/app.css");
 const TAILWIND_JS: Asset = asset!("/assets/tailwind/tailwind.js");
 const GOOGLE_ROBOT_FONT_CSS: Asset = asset!("/assets/google_material/google_robot_font.css");
-const GOOGLE_MATERIAL_CSS: Asset = asset!("/assets/google_material/google_material.css");
 const TAILWIND_CSS: Asset = asset!("/assets/tailwind/tailwind.css");
 
 #[component]
 pub fn App() -> Element {
     rsx! {
         document::Link { rel: "stylesheet", href: GOOGLE_ROBOT_FONT_CSS }
-        document::Link { rel: "stylesheet", href: GOOGLE_MATERIAL_CSS }
         document::Link { rel: "stylesheet", href: TAILWIND_CSS }
         document::Link { rel: "stylesheet", href: MAIN_CSS }
         document::Link { rel: "stylesheet", href: APP_CSS }
--- dioxus/src/bin/make.rs
+++ dioxus/src/bin/make.rs
@@ -1,28 +1,15 @@
 use std::env::current_dir;
-use std::fs::copy;
-use std::fs::read_dir;
-use std::path::Path;
 use std::process::Command;
+use std::fs;
+use std::path::Path;
 
 fn main() {
+    let target_dx_folder_path = Path::new(&current_dir().unwrap()).join("target").join("dx");
+    fs::remove_dir_all(target_dx_folder_path).unwrap();
     let _ = Command::new("dx")
         .args(["bundle", "--release", "--web"])
         .current_dir(current_dir().unwrap())
         .status()
         .unwrap()
         .success();
-    let assets_folder_path = Path::new(&current_dir().unwrap()).join("assets");
-    let assets_folder = read_dir(assets_folder_path).unwrap();
-    for assets_single_file in assets_folder {
-        if assets_single_file.as_ref().unwrap().path().is_file() {
-            let assets_target_file_path = Path::new(&current_dir().unwrap())
-                .join("target/dx/rust-app/release/web/public")
-                .join(assets_single_file.as_ref().unwrap().file_name());
-            copy(
-                assets_single_file.as_ref().unwrap().path(),
-                assets_target_file_path,
-            )
-            .unwrap();
-        }
-    }
 }
