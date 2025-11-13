55588a61fb484ee65f2ac0b980d548ab5ed0149b
John Williams <zdu.strong@gmail.com>
2025-11-13 18:25:52 +0900

    Remove tailwind-rs-dioxus dependency of dioxus project

--- dioxus/src/component/blog.rs
+++ dioxus/src/component/blog.rs
@@ -2,35 +2,14 @@ use crate::router::Route;
 use dioxus::prelude::*;
 use lumen_blocks::components::button::Button;
 use lumen_blocks::components::button::ButtonVariant;
-use tailwind_rs_dioxus::*;
 
 #[component]
 pub fn Blog(id: i128) -> Element {
-    let blog_css = ClassBuilder::new()
-        .margin(SpacingValue::Fractional(3.0))
-        .class("flex")
-        .class("flex-col")
-        .class("flex-auto")
-        .build_string();
-
-    let page_css = ClassBuilder::new()
-        .margin_left(SpacingValue::Fractional(3.0))
-        .margin_right(SpacingValue::Fractional(3.0))
-        .class("flex")
-        .class("flex-row")
-        .class("items-center")
-        .build_string();
-    let container_css = ClassBuilder::new()
-        .margin_top(SpacingValue::Fractional(3.0))
-        .class("flex")
-        .class("flex-row")
-        .build_string();
-
     rsx! {
 
         div {
             id: "blog",
-            class: blog_css,
+            class: "flex flex-col flex-auto m-3",
             h1 {
                 "This is blog #{id}!"
             }
@@ -38,7 +17,7 @@ pub fn Blog(id: i128) -> Element {
                 "In blog #{id}, we show how the Dioxus router works and how URL parameters can be passed as props to our route components."
             }
             div {
-                class: container_css,
+                class: "flex flex-row mt-3",
                 Link {
                     to: Route::BlogPage { id: id - 1 },
                     Button {
@@ -47,7 +26,7 @@ pub fn Blog(id: i128) -> Element {
                     }
                 }
                 div {
-                    class: page_css,
+                    class: "flex flex-row items-center ml-3 mr-3",
                     " <---> "
                 }
                 Link {
