786f1f8f26d88352dd9d90f8d27a739ee86ee75c
John Williams <zdu.strong@gmail.com>
2025-11-30 15:28:20 +0900

    Add sign_in method of dioxus project

---
+++ dioxus/src/api/authorization.rs
@@ -0,0 +1,6 @@
+use dioxus::prelude::*;
+
+pub async fn sign_in(username: Signal<String>, password: Signal<String>) {
+    let _ = username;
+    let _ = password;
+}
--- dioxus/src/main.rs
+++ dioxus/src/main.rs
@@ -1,4 +1,5 @@
 use crate::app::App;
+pub mod api;
 pub mod app;
 pub mod component;
 pub mod constant;
