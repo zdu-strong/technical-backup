8ad28a669eb9f401f89d4c286c7a87286da0cc7d
John Williams <zdu.strong@gmail.com>
2025-12-02 16:44:22 +0900

    Modify animal enum of dioxus project

--- dioxus/src/enums/animal_enum.rs
+++ dioxus/src/enums/animal_enum.rs
@@ -2,7 +2,7 @@ use strum_macros::AsRefStr;
 use strum_macros::EnumIter;
 use strum_macros::EnumString;
 
-#[derive(Debug, Clone, Copy, EnumIter, EnumString, AsRefStr)]
+#[derive(Debug, Clone, EnumIter, EnumString, AsRefStr)]
 pub enum AnimalEnum {
     #[strum(serialize = "TIGER")]
     TIGER,
--- rust/src/enums/animal_enum.rs
+++ rust/src/enums/animal_enum.rs
@@ -2,7 +2,7 @@ use strum_macros::AsRefStr;
 use strum_macros::EnumIter;
 use strum_macros::EnumString;
 
-#[derive(Debug, EnumIter, EnumString, AsRefStr)]
+#[derive(Debug, Clone, EnumIter, EnumString, AsRefStr)]
 pub enum AnimalEnum {
     #[strum(serialize = "TIGER")]
     TIGER,
