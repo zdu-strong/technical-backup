f482675fb3c24fae756b886750d667844952271c
John Williams <zdu.strong@gmail.com>
2025-12-02 17:08:00 +0900

    Modify PaginationModel of dioxus project

--- dioxus/src/model/pagination_model.rs
+++ dioxus/src/model/pagination_model.rs
@@ -24,29 +24,14 @@ where
     T: 'static + Serialize,
 {
     pub fn from(page_num: i128, page_size: i128, items: Vec<T>) -> PaginationModel<T> {
-        if page_num < 1 {
-            panic!("page_num cannot be less than 1")
-        }
-        if page_size < 1 {
-            panic!("page_size cannot be less than 1")
-        }
-        let total_records = items.len().to_i128().unwrap();
-        let total_pages =
-            (total_records / page_size) + (if total_records % page_size > 0 { 1 } else { 0 });
-        PaginationModel {
-            page_num: Signal::new(page_num),
-            page_size: Signal::new(page_size),
-            total_records: Signal::new(total_records),
-            total_pages: Signal::new(total_pages),
-            items: Signal::new(
-                items
-                    .into_iter()
-                    .skip(((page_num - 1) * page_size).to_usize().unwrap())
-                    .take(page_size.to_usize().unwrap())
-                    .map(|item| Signal::new(item))
-                    .collect::<Vec<Signal<_>>>(),
-            ),
-        }
+        PaginationModel::new(
+            page_num,
+            page_size,
+            items
+                .into_iter()
+                .map(|item| Signal::new(item))
+                .collect::<Vec<Signal<_>>>(),
+        )
     }
 
     pub fn new(page_num: i128, page_size: i128, items: Vec<Signal<T>>) -> PaginationModel<T> {
