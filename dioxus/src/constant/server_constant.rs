8caa8027cd4dc7e5d16637fabc4d62047ad551e9
John Williams <zdu.strong@gmail.com>
2025-12-12 08:05:49 +0900

    Modify set_server_user_info method of dioxus project

--- dioxus/src/constant/server_constant.rs
+++ dioxus/src/constant/server_constant.rs
@@ -19,21 +19,17 @@ pub const KEY_OF_SERVER_USER_INFO_OF_PERSISTENT: &str =
 pub fn remove_server_user_info() {
     spawn_forever_global_call(move || {
         *SERVER_USER_INFO.write() = UserModel::default();
-        let mut user_info_persistent =
-            new_persistent(KEY_OF_SERVER_USER_INFO_OF_PERSISTENT, || "".to_string());
-        if !user_info_persistent.read().is_empty() {
-            *user_info_persistent.write() = "".to_string();
+        if !get_server_user_info_persistent().is_empty() {
+            set_server_user_info_persistent("".to_string());
         }
     });
 }
 
 pub fn set_server_user_info(user: Option<Signal<UserModel>>) {
     let mut user_json_string = serde_json::to_string(&user).unwrap().to_string();
-    let has_param = user.is_some();
+    let has_param = user.is_some() && !user.unwrap().read().access_token.is_empty();
     if !has_param {
-        let user_info_persistent =
-            new_persistent(KEY_OF_SERVER_USER_INFO_OF_PERSISTENT, || "".to_string());
-        user_json_string = user_info_persistent.read().clone();
+        user_json_string = get_server_user_info_persistent();
         if user_json_string.is_empty() {
             remove_server_user_info();
             return;
@@ -43,9 +39,7 @@ pub fn set_server_user_info(user: Option<Signal<UserModel>>) {
         let user: UserModel = serde_json::from_str(user_json_string.as_str()).unwrap();
         *SERVER_USER_INFO.write() = user;
         if has_param {
-            let mut user_info_persistent =
-                new_persistent(KEY_OF_SERVER_USER_INFO_OF_PERSISTENT, || "".to_string());
-            *user_info_persistent.write() = user_json_string;
+            set_server_user_info_persistent(user_json_string);
         }
     });
 }
@@ -110,4 +104,16 @@ where
     })
     .poll_now();
     while !task.is_ready() {}
+}
+
+fn get_server_user_info_persistent() -> String {
+    let user_info_persistent =
+        new_persistent(KEY_OF_SERVER_USER_INFO_OF_PERSISTENT, || "".to_string());
+    return user_info_persistent.read().clone();
+}
+
+fn set_server_user_info_persistent(user_json_string: String) {
+    let mut user_info_persistent =
+        new_persistent(KEY_OF_SERVER_USER_INFO_OF_PERSISTENT, || "".to_string());
+    *user_info_persistent.write() = user_json_string;
 }
