6684a65f66a82619f825f0137ad522ab4f807182
John Williams <zdu.strong@gmail.com>
2025-12-16 15:38:40 +0900

    Modify main menu of react project

--- react/src/component/SystemMenu/MainMenu.tsx
+++ react/src/component/SystemMenu/MainMenu.tsx
@@ -4,6 +4,16 @@ import { DashboardLayout } from '@toolpad/core/DashboardLayout';
 import { ReactRouterAppProvider } from '@toolpad/core/react-router'
 import UserInfoMenu from '@component/SystemMenu/UserInfoMenu';
 import { useReactRouterAppProviderNavigation } from '@component/SystemMenu/js/useReactRouterAppProviderNavigation';
+import { style } from 'typestyle';
+
+const dashboardLayoutContainer = style({
+    $nest: {
+        "& > div.MuiBox-root": {
+            width: "100%",
+            height: "100%"
+        }
+    }
+});
 
 type Props = {
     children: ReactNode;
@@ -21,16 +31,18 @@ export default observer((props: Props) => {
         navigation={state.navigation}
         branding={{ title: "", logo: "" }}
     >
-        <DashboardLayout
-            slots={{ toolbarActions: UserInfoMenu }}
-        >
-            <div className="overflow-auto flex flex-auto">
-                <div className="flex flex-col min-w-full min-h-full w-max">
-                    <div className='flex flex-col flex-auto'>
-                        {props.children}
+        <div className={`flex flex-col flex-auto ${dashboardLayoutContainer}`}>
+            <DashboardLayout
+                slots={{ toolbarActions: UserInfoMenu }}
+            >
+                <div className="overflow-auto flex flex-auto">
+                    <div className="flex flex-col min-w-full min-h-full w-max">
+                        <div className='flex flex-col flex-auto'>
+                            {props.children}
+                        </div>
                     </div>
                 </div>
-            </div>
-        </DashboardLayout>
+            </DashboardLayout>
+        </div>
     </ReactRouterAppProvider>
 })
