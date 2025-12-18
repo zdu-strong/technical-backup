cae1b656647d7a12e39bf861c1d03ae767b43e49
John Williams <zdu.strong@gmail.com>
2025-12-18 16:10:37 +0900

    Modify use_once_submit hooks of dioxus project

--- dioxus/src/constant/server_constant.rs
+++ dioxus/src/constant/server_constant.rs
@@ -2,10 +2,13 @@ use crate::model::user_model::UserModel;
 use chrono::Local;
 use dioxus::prelude::*;
 use dioxus_sdk::storage::new_persistent;
+use futures::Future;
+use futures::FutureExt;
 use reqwest::Client;
 use reqwest::Method;
 use reqwest::RequestBuilder;
 use reqwest::Url;
+use std::panic::UnwindSafe;
 use uuid::Uuid;
 
 pub const SERVER_ADDRESS: GlobalSignal<String> = GlobalSignal::new(|| get_server_address());
@@ -14,6 +17,8 @@ pub const SERVER_USER_INFO: GlobalSignal<UserModel> = GlobalSignal::new(|| UserM
 
 pub const KEY_OF_SERVER_USER_INFO_OF_PERSISTENT: &str =
     "GlobalUserInfo-5a8dcb9f-495f-4894-8dcb-9f495f489409";
+
+pub const SERVER_ERROR: GlobalSignal<Option<String>> = GlobalSignal::new(|| None);
 
 pub fn remove_server_user_info() {
     spawn_forever_global_call(move || {
@@ -59,16 +64,144 @@ pub fn delete(url: &str) -> RequestBuilder {
     get_request_builder(Method::DELETE, url)
 }
 
-pub fn use_multiple_query() -> HookStatusModel {
-    return HookStatusModel::default();
+pub fn use_multiple_query<F>(
+    mut future: impl FnMut() -> F + UnwindSafe + 'static,
+) -> Signal<HookStatusModel>
+where
+    F: Future + UnwindSafe + 'static,
+{
+    let mut hook_status = use_signal(|| HookStatusModel::default());
+
+    let callback = use_callback(move |_| {
+        let fut = future();
+        dioxus_core::spawn(async move {
+            {
+                hook_status.write().loading = true;
+            }
+            match fut.catch_unwind().await {
+                Ok(_) => {
+                    hook_status.write().ready = true;
+                    hook_status.write().loading = false;
+                    hook_status.write().error = None;
+                }
+                Err(_) => {
+                    hook_status.write().loading = false;
+                    hook_status.write().error = Some("Problem".to_string());
+                }
+            }
+        })
+    });
+
+    // Create the task inside a CopyValue so we can reset it in-place later
+    let task = use_hook(|| CopyValue::new(callback(())));
+
+    // Early returns in dioxus have consequences for use_memo, use_resource, and use_future, etc
+    // We *don't* want futures to be running if the component early returns. It's a rather weird behavior to have
+    // use_memo running in the background even if the component isn't hitting those hooks anymore.
+    //
+    // React solves this by simply not having early returns interleave with hooks.
+    // However, since dioxus allows early returns (since we use them for suspense), we need to solve this problem
+    use_hook_did_run(move |did_run| match did_run {
+        true => task.peek().resume(),
+        false => task.peek().pause(),
+    });
+
+    hook_status
 }
 
-pub fn use_multiple_submit() -> HookStatusModel {
-    return HookStatusModel::default();
+pub fn use_multiple_submit<F>(
+    mut future: impl FnMut() -> F + UnwindSafe + 'static,
+) -> Signal<HookStatusModel>
+where
+    F: Future + UnwindSafe + 'static,
+{
+    let mut hook_status = use_signal(|| HookStatusModel::default());
+
+    let callback = use_callback(move |_| {
+        let fut = future();
+        dioxus_core::spawn(async move {
+            {
+                hook_status.write().loading = true;
+            }
+            match fut.catch_unwind().await {
+                Ok(_) => {
+                    hook_status.write().ready = true;
+                    hook_status.write().loading = false;
+                    hook_status.write().error = None;
+                }
+                Err(_) => {
+                    *SERVER_ERROR.write() = Some("Problem".to_string());
+                    hook_status.write().loading = false;
+                    hook_status.write().error = Some("Problem".to_string());
+                }
+            }
+        })
+    });
+
+    // Create the task inside a CopyValue so we can reset it in-place later
+    let task = use_hook(|| CopyValue::new(callback(())));
+
+    // Early returns in dioxus have consequences for use_memo, use_resource, and use_future, etc
+    // We *don't* want futures to be running if the component early returns. It's a rather weird behavior to have
+    // use_memo running in the background even if the component isn't hitting those hooks anymore.
+    //
+    // React solves this by simply not having early returns interleave with hooks.
+    // However, since dioxus allows early returns (since we use them for suspense), we need to solve this problem
+    use_hook_did_run(move |did_run| match did_run {
+        true => task.peek().resume(),
+        false => task.peek().pause(),
+    });
+
+    hook_status
 }
 
-pub fn use_once_submit() -> HookStatusModel {
-    return HookStatusModel::default();
+pub fn use_once_submit<F>(
+    mut future: impl FnMut() -> F + UnwindSafe + 'static,
+) -> Signal<HookStatusModel>
+where
+    F: Future + UnwindSafe + 'static,
+{
+    let mut hook_status = use_signal(|| HookStatusModel::default());
+
+    let callback = use_callback(move |_| {
+        let fut = future();
+        dioxus_core::spawn(async move {
+            if hook_status.read().ready {
+                return;
+            }
+            {
+                hook_status.write().loading = true;
+            }
+            match fut.catch_unwind().await {
+                Ok(_) => {
+                    hook_status.write().ready = true;
+                    hook_status.write().loading = false;
+                    hook_status.write().error = None;
+                }
+                Err(_) => {
+                    *SERVER_ERROR.write() = Some("Problem".to_string());
+                    hook_status.write().loading = false;
+                    hook_status.write().error = Some("Problem".to_string());
+                }
+            }
+        })
+    });
+
+    // Create the task inside a CopyValue so we can reset it in-place later
+    let task = use_hook(|| CopyValue::new(callback(())));
+
+    // Early returns in dioxus have consequences for use_memo, use_resource, and use_future, etc
+    // We *don't* want futures to be running if the component early returns. It's a rather weird behavior to have
+    // use_memo running in the background even if the component isn't hitting those hooks anymore.
+    //
+    // React solves this by simply not having early returns interleave with hooks.
+    // However, since dioxus allows early returns (since we use them for suspense), we need to solve this problem
+    use_hook_did_run(move |did_run| match did_run {
+        true => task.peek().resume(),
+        false => task.peek().pause(),
+    });
+
+    hook_status
 }
 
 fn get_request_builder(method: Method, url: &str) -> RequestBuilder {
@@ -135,9 +268,9 @@ fn get_server_address() -> String {
 
 #[derive(Debug, Clone, Default)]
 pub struct HookStatusModel {
-    pub loading: Signal<bool>,
-    pub ready: Signal<bool>,
-    pub error: Option<Signal<String>>,
+    pub loading: bool,
+    pub ready: bool,
+    pub error: Option<String>,
     pub hook_callback: Option<Signal<Callback<()>>>,
 }
 
