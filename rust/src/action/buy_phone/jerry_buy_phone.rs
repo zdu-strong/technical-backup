d03611528974e845f3217147e451e5053ec05742
John Williams <zdu.strong@gmail.com>
2026-01-03 20:51:14 +0900

    Modify buy trait example of rust project

--- rust/src/action/buy_phone/jerry_buy_phone.rs
+++ rust/src/action/buy_phone/jerry_buy_phone.rs
@@ -2,25 +2,23 @@ use crate::model::pixel_model::Buy;
 use crate::model::pixel_model::PixelModel;
 use futures::prelude::*;
 use futures::stream::iter;
-use std::sync::Arc;
 use tokio::runtime::Handle;
-use tokio::sync::RwLock;
 use tokio::task::spawn_blocking;
 
 pub async fn jerry_buy_phone() {
     let phone_list = vec![
-        Arc::new(RwLock::new(Box::new(PixelModel {
+        Box::new(PixelModel {
             price: "10,000".to_string(),
             owner: "Jerry".to_string(),
-        }) as Box<dyn Buy>)),
-        Arc::new(RwLock::new(Box::new(PixelModel {
+        }) as Box<dyn Buy>,
+        Box::new(PixelModel {
             price: "3,000".to_string(),
             owner: "Jerry".to_string(),
-        }))),
+        }),
     ];
     let _ = iter(phone_list)
-        .map(move |phone| async move {
-            phone.write().await.buy().await;
+        .map(move |mut phone| async move {
+            phone.buy().await;
         })
         .map(|s| async {
             spawn_blocking(|| Handle::current().block_on(s))
