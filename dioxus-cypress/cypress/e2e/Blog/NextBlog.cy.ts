8684ebd00c1be72f46a9c2249099cb7480db871d
John Williams <zdu.strong@gmail.com>
2026-01-05 17:49:53 +0900

    Modify e2e tests of dioxus project

--- dioxus-cypress/cypress/e2e/Blog/NextBlog.cy.ts
+++ dioxus-cypress/cypress/e2e/Blog/NextBlog.cy.ts
@@ -3,7 +3,7 @@ import page from '@/page'
 it('', () => {
     page.Blog.NextButton().click()
     page.Blog.BlogTitile().should("have.text", "This is blog #2!")
-    cy.url().should("include", "/blog/2")
+    cy.location('pathname').should('equal', "/blog/2")
 })
 
 before(() => {
--- dioxus-cypress/cypress/e2e/Blog/PreviousBlog.cy.ts
+++ dioxus-cypress/cypress/e2e/Blog/PreviousBlog.cy.ts
@@ -3,7 +3,7 @@ import page from '@/page'
 it('', () => {
     page.Blog.PreviousButton().click()
     page.Blog.BlogTitile().should("have.text", "This is blog #0!")
-    cy.url().should("include", "/blog/0")
+    cy.location('pathname').should('equal', "/blog/0")
 })
 
 before(() => {
