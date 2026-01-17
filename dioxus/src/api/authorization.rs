e508d6cb6bd9442b0a42f0f5cb988a758be6db13
John Williams <zdu.strong@gmail.com>
2026-01-18 08:11:07 +0900

    Modify server_constant of dioxus project

--- dioxus/src/api/authorization.rs
+++ dioxus/src/api/authorization.rs
@@ -19,7 +19,7 @@ pub async fn sign_in(username: Signal<String>, password: Signal<String>) {
 }
 
 pub async fn sign_out() {
-    if !SERVER_USER_INFO().access_token.is_empty() {
+    if !SERVER_USER_INFO().access_token.read().is_empty() {
         let _ = post("/sign-out").send().await;
         remove_server_user_info();
     }
