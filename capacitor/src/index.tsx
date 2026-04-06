f5fb1070488945d7c5225fe05ee8aa68af35f5eb
John Williams <zdu.strong@gmail.com>
2026-04-07 08:28:29 +0900

    feat: add jetbrains-mono font

--- capacitor/src/index.tsx
+++ capacitor/src/index.tsx
@@ -1,12 +1,13 @@
-import 'tailwindcss/utilities.css';
-import '@common/app-base-css/index.css';
-import '@fontsource/roboto';
-import '@common/Server';
-import '@common/ScreenOrentation';
-import '@common/StorageManage/StorageManageRun';
-import ReactDom from 'react-dom/client'
-import App from '@/App';
+import "tailwindcss/utilities.css";
+import "@common/app-base-css/index.css";
+import "@fontsource/jetbrains-mono";
+import "@fontsource/roboto";
+import "@common/Server";
+import "@common/ScreenOrentation";
+import "@common/StorageManage/StorageManageRun";
+import ReactDom from "react-dom/client"
+import App from "@/App";
 
-ReactDom.createRoot(document.getElementById('root')!).render(
+ReactDom.createRoot(document.getElementById("root")!).render(
     <App />
 )
--- electron/src/index.tsx
+++ electron/src/index.tsx
@@ -1,11 +1,12 @@
-import 'tailwindcss/utilities.css';
-import '@common/app-base-css/index.css';
-import '@fontsource/roboto';
-import '@common/Server';
-import '@common/StorageManage/StorageManageRun';
-import ReactDom from 'react-dom/client'
-import App from '@/App';
+import "tailwindcss/utilities.css";
+import "@common/app-base-css/index.css";
+import "@fontsource/jetbrains-mono";
+import "@fontsource/roboto";
+import "@common/Server";
+import "@common/StorageManage/StorageManageRun";
+import ReactDom from "react-dom/client"
+import App from "@/App";
 
-ReactDom.createRoot(document.getElementById('root')!).render(
+ReactDom.createRoot(document.getElementById("root")!).render(
     <App />
 )
