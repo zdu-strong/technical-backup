642bc226bceba21954d602e9cdfdbcd6e8897360
John Williams <zdu.strong@gmail.com>
2026-05-01 17:15:22 +0900

    feat: modify enums

--- dioxus/src/enums/animal_enum.rs
+++ dioxus/src/enums/animal_enum.rs
@@ -1,11 +1,9 @@
 use derive_more::Display;
-use serde::Deserialize;
-use serde::Serialize;
 use strum_macros::AsRefStr;
 use strum_macros::EnumIter;
 use strum_macros::EnumString;
 
-#[derive(Debug, Display, Clone, Serialize, Deserialize, EnumIter, EnumString, AsRefStr)]
+#[derive(Debug, Display, Clone, EnumIter, EnumString, AsRefStr)]
 #[display("{}", self.as_ref())]
 pub enum AnimalEnum {
     #[strum(serialize = "TIGER")]
