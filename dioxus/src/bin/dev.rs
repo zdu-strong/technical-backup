e2c627e16ec2a3527a8157ba2021c45bb7f73ca9
John Williams <zdu.strong@gmail.com>
2026-07-02 06:38:24 +0900

    feat: modify listen ip in the development environment of dioxus project

--- dioxus/src/bin/dev.rs
+++ dioxus/src/bin/dev.rs
@@ -17,6 +17,8 @@ fn main() {
             "web",
             "--open",
             "true",
+            "--addr",
+            "0.0.0.0",
             "--port",
             "3000",
             "--hot-reload",
