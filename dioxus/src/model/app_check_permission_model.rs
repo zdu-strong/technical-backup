debf45cb8af7eaf1aa1eb5b7e537859314edfe4b
John Williams <zdu.strong@gmail.com>
2026-07-04 17:21:16 +0900

    feat: modify AppCheckPermissionModel of dioxus project

--- dioxus/src/model/app_check_permission_model.rs
+++ dioxus/src/model/app_check_permission_model.rs
@@ -20,13 +20,13 @@ pub struct AppCheckPermissionModel {
 }
 
 impl AppCheckPermissionModel {
-    pub fn is_ready(&self, ready: bool) -> bool {
+    pub fn is_ready(&self, readyOfInit: bool) -> bool {
         if self.check_is_sign_in && SERVER_USER_INFO().access_token.read().trim().is_empty() {
             return false;
         }
         if self.check_is_not_sign_in && !SERVER_USER_INFO().access_token.read().trim().is_empty() {
             return false;
         }
-        return ready && true;
+        return readyOfInit && true;
     }
 }
