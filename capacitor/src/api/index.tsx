4fcecfcdb551b46f51c16b1b40f00170bd936d5c
John Williams <zdu.strong@gmail.com>
2026-07-07 04:53:18 +0900

    feat: remove ts-enum-util dependency of electron project

--- capacitor/src/api/index.tsx
+++ capacitor/src/api/index.tsx
@@ -1,9 +1,13 @@
 import * as Git from '@api/Git'
 import * as Resource from '@api/Resource'
 import * as LongTermTask from '@api/LongTermTask'
+import * as Authorization from '@api/Authorization';
+import * as EncryptDecrypt from '@api/EncryptDecrypt';
 
 export default {
     Git,
     Resource,
     LongTermTask,
+    Authorization,
+    EncryptDecrypt,
 }
