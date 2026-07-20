cc8c4f5bbb2bac8a7b26181d4993b41eed4e3674
John Williams <zdu.strong@gmail.com>
2026-07-21 08:29:40 +0900

    Revert "feat: modify example of eat apple of rust project"
    This reverts commit 3db6cef34f4c6ddbbcbac810cd50763e279a95cc.

--- rust/src/action/eat_food/tom_eat_apple.rs
+++ rust/src/action/eat_food/tom_eat_apple.rs
@@ -1,8 +1,8 @@
 use crate::action::eat_food::eat_apple::eat_apple;
 
 pub async fn tom_eat_apple() {
-    let username = "tom".to_string();
-    let max_apple_count = 10;
+    let ref mut username = "tom".to_string();
+    let ref mut max_apple_count = 10;
     let ref mut surplus_apple_count = max_apple_count.clone();
     eat_apple(username, surplus_apple_count, max_apple_count).await;
 }
