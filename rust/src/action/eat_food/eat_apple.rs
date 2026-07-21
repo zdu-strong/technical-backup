55addefe411cfe34f461951c9cd6e5099d767c7a
John Williams <zdu.strong@gmail.com>
2026-07-21 18:59:51 +0900

    feat: modify eat_apple example of rust project

--- rust/src/action/eat_food/eat_apple.rs
+++ rust/src/action/eat_food/eat_apple.rs
@@ -1,23 +1,20 @@
 use std::collections::VecDeque;
-use std::sync::Arc;
-use tokio::sync::RwLock;
 
 pub async fn eat_apple(
-    username: String,
-    surplus_apple_count: Arc<RwLock<i128>>,
-    max_apple_count: i128,
+    username: &mut String,
+    surplus_apple_count: &mut i128,
+    max_apple_count: &mut i128,
 ) {
-    let ref mut surplus_apple_count = *surplus_apple_count.write().await;
     let ref mut vec_deque = VecDeque::from([(username, surplus_apple_count, max_apple_count)]);
     while !vec_deque.is_empty() {
         let (username, surplus_apple_count, max_apple_count) = vec_deque.pop_front().unwrap();
-        if max_apple_count < 0 {
+        if *max_apple_count < 0 {
             panic!("illegal max apple count");
         }
         if *surplus_apple_count < 0 {
             panic!("illegal surplus apple count");
         }
-        if *surplus_apple_count > max_apple_count {
+        if *surplus_apple_count > *max_apple_count {
             panic!("illegal surplus apple count");
         }
         if *surplus_apple_count == 0 {
@@ -27,8 +24,8 @@ pub async fn eat_apple(
         println!(
             "{} eat {}{} {}",
             username,
-            max_apple_count - *surplus_apple_count,
-            match max_apple_count - *surplus_apple_count {
+            *max_apple_count - *surplus_apple_count,
+            match *max_apple_count - *surplus_apple_count {
                 1 => "st",
                 2 => "nd",
                 3 => "rd",
