62b23056de6a5f4f68ba26b1935e4c2005cded08
John Williams <zdu.strong@gmail.com>
2026-07-21 16:56:45 +0900

    feat: modify tom_eat_apple example of rust project

--- rust/src/action/eat_food/tom_eat_apple.rs
+++ rust/src/action/eat_food/tom_eat_apple.rs
@@ -1,8 +1,10 @@
 use crate::action::eat_food::eat_apple::eat_apple;
+use std::sync::Arc;
+use tokio::sync::RwLock;
 
 pub async fn tom_eat_apple() {
-    let ref mut username = "tom".to_string();
-    let ref mut max_apple_count = 10;
-    let ref mut surplus_apple_count = max_apple_count.clone();
+    let username = "tom".to_string();
+    let max_apple_count = 10;
+    let surplus_apple_count = Arc::new(RwLock::new(max_apple_count));
     eat_apple(username, surplus_apple_count, max_apple_count).await;
 }
