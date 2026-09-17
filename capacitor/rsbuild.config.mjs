c1afb943aa0c86c59675168fc42b0125972d5c9a
John Williams <zdu.strong@gmail.com>
2026-09-17 20:02:30 +0900

    feat: add mlkem dependency of capacitorjs project

--- capacitor/rsbuild.config.mjs
+++ capacitor/rsbuild.config.mjs
@@ -59,5 +59,9 @@ export default defineConfig({
                 strategy: "single-vendor",
             },
         },
+        client: {
+            logLevel: "silent",
+            overlay: false
+        }
     },
 });
